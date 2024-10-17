b5d652adc104127af1608f145dbb9f54747f183b
belalelnady <belalelnadyz@gmail.com>
2024-10-17 16:22:49 +0300

    modified:   the inventory

--- Jenkinsfile
+++ Jenkinsfile
@@ -17,19 +17,19 @@ pipeline {
                 
             }
         }
-        stage('Install') {
-                steps {
-                    //  Install dependencies once
-                    sh 'npm  --prefix web-app ci'   
-                }
-            }
+        // stage('Install') {
+        //         steps {
+        //             //  Install dependencies once
+        //             sh 'npm  --prefix web-app ci'   
+        //         }
+        //     }
     
-        stage('Test') {
-                    steps {
-                        // Test the web app before building it with jest
-                       sh 'npm --prefix web-app test'
-                    }
-                }
+        // stage('Test') {
+        //             steps {
+        //                 // Test the web app before building it with jest
+        //                sh 'npm --prefix web-app test'
+        //             }
+        //         }
 
 
         stage('Build Docker Image') {
--- terraform/main.tf
+++ terraform/main.tf
@@ -7,38 +7,38 @@ module "vpc" {
 
 # subnets
 module "public_subnet1" {
-  source      = "./subnet"
-  vpc_id      = module.vpc.vpc_id
-  cidr = "10.0.1.0/24"
+  source            = "./subnet"
+  vpc_id            = module.vpc.vpc_id
+  cidr              = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-  subnet_name = "public-subnet1"
-  map_on_launch = true
+  subnet_name       = "public-subnet1"
+  map_on_launch     = true
 }
 
 
 module "public_subnet2" {
-  source      = "./subnet"
-  vpc_id      = module.vpc.vpc_id
-  cidr = "10.0.2.0/24"
+  source            = "./subnet"
+  vpc_id            = module.vpc.vpc_id
+  cidr              = "10.0.2.0/24"
   availability_zone = "us-east-1b"
-  subnet_name = "public-subnet2"
-  map_on_launch = true
+  subnet_name       = "public-subnet2"
+  map_on_launch     = true
 }
 module "private_subnet1" {
-  source      = "./subnet"
-  vpc_id      = module.vpc.vpc_id
-  cidr = "10.0.3.0/24"
+  source            = "./subnet"
+  vpc_id            = module.vpc.vpc_id
+  cidr              = "10.0.3.0/24"
   availability_zone = "us-east-1a"
-  subnet_name = "private-subnet1"
-  map_on_launch = false
+  subnet_name       = "private-subnet1"
+  map_on_launch     = false
 }
 module "private_subnet2" {
-  source      = "./subnet"
-  vpc_id      = module.vpc.vpc_id
-  cidr = "10.0.4.0/24"
+  source            = "./subnet"
+  vpc_id            = module.vpc.vpc_id
+  cidr              = "10.0.4.0/24"
   availability_zone = "us-east-1b"
-  subnet_name = "private-subnet2"
-  map_on_launch = false
+  subnet_name       = "private-subnet2"
+  map_on_launch     = false
 }
 
 
@@ -74,30 +74,30 @@ module "private_routing_table" {
 
 #  subnet  assossiation
 module "public_assosiation_1" {
-  source               = "./association"
-  subnet_id            = module.public_subnet1.subnet_id
-  route_table_id       = module.public_routing_table.routing_table_id
+  source         = "./association"
+  subnet_id      = module.public_subnet1.subnet_id
+  route_table_id = module.public_routing_table.routing_table_id
 }
 
 module "public_assosiation_2" {
-  source               = "./association"
-  subnet_id            = module.public_subnet2.subnet_id
-  route_table_id       = module.public_routing_table.routing_table_id
+  source         = "./association"
+  subnet_id      = module.public_subnet2.subnet_id
+  route_table_id = module.public_routing_table.routing_table_id
 }
 
 
 module "private_assosiation_1" {
-  source               = "./association"
-  subnet_id            = module.private_subnet1.subnet_id
-  route_table_id       = module.private_routing_table.routing_table_id
+  source         = "./association"
+  subnet_id      = module.private_subnet1.subnet_id
+  route_table_id = module.private_routing_table.routing_table_id
 }
 
 
 
 module "private_assosiation_2" {
-  source               = "./association"
-  subnet_id            = module.private_subnet2.subnet_id
-  route_table_id       = module.private_routing_table.routing_table_id
+  source         = "./association"
+  subnet_id      = module.private_subnet2.subnet_id
+  route_table_id = module.private_routing_table.routing_table_id
 }
 
 
@@ -116,15 +116,15 @@ module "key_pair" {
 
 # instances
 module "jenkins-agent" {
-  source                    = "./public_instance"
-  ami_id                    = "ami-0a0e5d9c7acc336f1" 
-  instance_type             = "t2.micro"
-  instance_name             = "jenkins-agent"
-  public_subnet_id          = module.public_subnet1.subnet_id
-  public_sg_id              = module.security_groups.public_sg_id
-  depends_on = [ module.internet_gateway ]
-  key_name                  = module.key_pair.key_name
-  user_data = <<-EOF
+  source           = "./public_instance"
+  ami_id           = "ami-0a0e5d9c7acc336f1"
+  instance_type    = "t2.micro"
+  instance_name    = "jenkins-agent"
+  public_subnet_id = module.public_subnet1.subnet_id
+  public_sg_id     = module.security_groups.public_sg_id
+  depends_on       = [module.internet_gateway]
+  key_name         = module.key_pair.key_name
+  user_data        = <<-EOF
               #!/bin/bash
               sudo apt update -y
               sudo apt install -y python3-pip
@@ -139,15 +139,15 @@ module "jenkins-agent" {
 
 }
 module "jenkins_master" {
-  source                    = "./public_instance"
-  ami_id                    = "ami-0a0e5d9c7acc336f1" 
-  instance_type             = "t2.micro"
-  instance_name             = "jenkins-master"
-  public_subnet_id          = module.public_subnet2.subnet_id
-  public_sg_id              = module.security_groups.public_sg_id
-  depends_on = [ module.internet_gateway ]
-  key_name                  = module.key_pair.key_name
-  user_data = <<-EOF
+  source           = "./public_instance"
+  ami_id           = "ami-0a0e5d9c7acc336f1"
+  instance_type    = "t2.micro"
+  instance_name    = "jenkins-master"
+  public_subnet_id = module.public_subnet2.subnet_id
+  public_sg_id     = module.security_groups.public_sg_id
+  depends_on       = [module.internet_gateway]
+  key_name         = module.key_pair.key_name
+  user_data        = <<-EOF
               #!/bin/bash
               sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
               https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
@@ -164,40 +164,40 @@ module "jenkins_master" {
 }
 
 module "web-app1" {
-  source                    = "./private_instance"
-  ami_id                    = "ami-0a0e5d9c7acc336f1" 
-  instance_type             = "t2.micro"
-  instance_name             = "web-app1"
-  private_subnet_id          = module.private_subnet1.subnet_id
-  private_sg_id              = module.security_groups.private_sg_id
-  depends_on = [ module.nat ]
-  key_name                  = module.key_pair.key_name
-  user_data = ""
+  source            = "./private_instance"
+  ami_id            = "ami-0a0e5d9c7acc336f1"
+  instance_type     = "t2.micro"
+  instance_name     = "web-app1"
+  private_subnet_id = module.private_subnet1.subnet_id
+  private_sg_id     = module.security_groups.private_sg_id
+  depends_on        = [module.nat]
+  key_name          = module.key_pair.key_name
+  user_data         = ""
 
 }
 
 module "web-app2" {
-  source                    = "./private_instance"
-  ami_id                    = "ami-0a0e5d9c7acc336f1" 
-  instance_type             = "t2.micro"
-  instance_name             = "web-app2"
-  private_subnet_id          = module.private_subnet2.subnet_id
-  private_sg_id              = module.security_groups.private_sg_id
-  depends_on = [ module.nat ]
-  key_name                  = module.key_pair.key_name
-  user_data = ""
+  source            = "./private_instance"
+  ami_id            = "ami-0a0e5d9c7acc336f1"
+  instance_type     = "t2.micro"
+  instance_name     = "web-app2"
+  private_subnet_id = module.private_subnet2.subnet_id
+  private_sg_id     = module.security_groups.private_sg_id
+  depends_on        = [module.nat]
+  key_name          = module.key_pair.key_name
+  user_data         = ""
 }
 
 
 # Load Balancer
 module "load_balancer" {
-  source                = "./load_balancer"
-  vpc_id                = module.vpc.vpc_id
-  public_subnets_id        = [module.public_subnet1.subnet_id , module.public_subnet2.subnet_id]
+  source            = "./load_balancer"
+  vpc_id            = module.vpc.vpc_id
+  public_subnets_id = [module.public_subnet1.subnet_id, module.public_subnet2.subnet_id]
 
-  private_id_1= module.web-app1.private_instance_id
+  private_id_1 = module.web-app1.private_instance_id
   private_id_2 = module.web-app2.private_instance_id
-  alb_sg_id             = module.security_groups.alb_sg_id
+  alb_sg_id    = module.security_groups.alb_sg_id
 }
 
 
