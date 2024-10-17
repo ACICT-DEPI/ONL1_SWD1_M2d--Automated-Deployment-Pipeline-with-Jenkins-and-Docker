ddb739fd4fbc694f12f203d2d715c504974c560f
salamaxx97 <mohamedsalama20072@gmail.com>
2024-10-17 16:02:32 +0300

    Update APP

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,15 +1,34 @@
-import logo from './logo.svg';
-import cat from './exhuasted.jpg';
+import React from 'react';
 import './App.css';
+import infraImage from './infrastructure.png'; // Importing the image
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="center">The sad reality of the DevOps</h1>
-        <p className="center">You have to do just about everything, and somehow you do nothing at the same time</p>
-        <img src={cat} style={{"margin-top" : "50vh"}} alt="cat" />
+        <h1>Welcome to Our CI/CD Pipeline Project</h1>
+        <p>
+          This project implements a fully automated CI/CD pipeline using Terraform, Jenkins, Docker, 
+          and Ansible, with cloud deployment.
+        </p>
+
+        <h2>Technologies Used</h2>
+        <ul>
+          <li>Jenkins</li>
+          <li>Docker</li>
+          <li>Ansible</li>
+          <li>Terraform</li>
+        </ul>
+
+        {/* Infrastructure Image */}
+        <h2>Infrastructure Overview</h2>
+        <img src={infraImage} alt="Infrastructure Diagram" className="infra-image" />
+
+        {/* Created By Section */}
+        <footer>
+          <h3>Created by: </h3>
+          <p>Belala Nady, Ahmed Abdelhares, Aya Alaa, Ahmed Darder, Mohamed Salama , Asmhan Salah</p>
+        </footer>
       </header>
     </div>
   );
